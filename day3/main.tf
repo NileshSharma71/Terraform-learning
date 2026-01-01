76afd0a827a3920fc661342f8ac07c67bdb7362e
Nilesh Sharma <sharmanilesh7105@gmail.com>
2026-01-02 03:15:28 +0530

    modified main and outputs file of day3

--- day3/main.tf
+++ day3/main.tf
@@ -7,3 +7,10 @@ module "ec2" {
   ami = "ami-00ca570c1b6d79f36"
   instance_type = "t2.micro"
 }
+
+module "s3" {
+  source = "./modules/s3"
+  bucket_name = "unique-bucket-name-7105"
+  keyvalue = "readmefile"
+  sourcename = "../README.md"
+}
--- day3/outputs.tf
+++ day3/outputs.tf
@@ -1,3 +1,9 @@
 output "public_ip" {
   value = module.ec2.public_ip
 }
+output "bucket_arn" {
+  value = module.s3.bucket_arn
+}
+output "s3_console_link" {
+  value = module.s3.console_link
+}
